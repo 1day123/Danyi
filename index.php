09488d88afa68f7231afa7a4ff3e04f02111064c
Dayra Sharay <143970278+1day123@users.noreply.github.com>
2024-04-13 23:33:21 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,53 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="cliente/css/estilos.css">
+    <title>Hello, world!</title>
+</head>
+
+<body>
+
+
+    <div class="container-fluid">
+
+        <video muted autoplay loop class="fondo">
+            <source src="./cliente/videos/video-index.mp4" type="video/mp4">
+        </video>
+
+        <header class="relative justify-rigth">
+            <div>
+                <img src="Cliente/img/logo-principal.png" alt="" id="logo">
+            </div>
+            <nav class="nav navbar-expand-lg margin-top-3">
+                <a class="nav-link text-nav btn-g background-rojo radius center" href="cliente/Interfaz-cliente/menu-navegacion-cliente.php">Registrarse</a>
+            </nav>
+        </header>
+        <div>
+            <h1 id="text-Dulces-index" class="absolute txt-blanco txt-g left">Dulces de México</h1>
+            <h4 id="text-sub-index" class="absolute txt-blanco txt-m left">Descubre la magia del dulce en cada mordisco!</h4>
+            <button id="btn-comenzar-index" class=" absolute text-nav btn-eg background-rojo radius center left">Comenzar</button>
+        </div>
+
+    </div>
+
+
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
